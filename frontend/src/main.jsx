3c111456fa6832a313df7690ecd5f911512a1c7b
sai-manikanta <116022819+7036143077@users.noreply.github.com>
2025-06-19 20:39:21 +0530

    App.jsx changed to App

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import './index.css'
-import App from './App.jsx'
+import App from './App'
 import {BrowserRouter} from 'react-router-dom'
 import ReactDOM from 'react-dom/client'
 import ShopContextProvider from './context/ShopContext.jsx'
