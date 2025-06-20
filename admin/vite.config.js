56e2062df51da31738ba3141e157355cb8ea0f3a
sai-manikanta <116022819+7036143077@users.noreply.github.com>
2025-06-20 11:28:18 +0530

    config.js

--- admin/vite.config.js
+++ admin/vite.config.js
@@ -4,5 +4,12 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: './',
+  build: {
+    outDir: 'dist',
+    rollupOptions: {
+      external: ['react-toastify', 'react-router-dom']
+    }
+  },
   server: {port : 5174}
 })
