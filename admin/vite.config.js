2f86364d37c71fda3757eec5b6bea03d57b2f6db
sai-manikanta <116022819+7036143077@users.noreply.github.com>
2025-06-20 11:21:44 +0530

    This may the last one

--- admin/vite.config.js
+++ admin/vite.config.js
@@ -1,11 +1,8 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      external: ['react-toastify', 'react-router-dom']
-    }
-  }
-});
+  server: {port : 5174}
+})
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,13 +1,20 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vite.dev/config/
 export default defineConfig({
-  base: './', 
   plugins: [react()],
+  base: './',
   build: {
     outDir: 'dist',
     rollupOptions: {
       external: ['react-toastify', 'react-router-dom']
     }
-  }
-});
+  },
+  
+  server : {port : 5173}
+})
+
+
+
+
