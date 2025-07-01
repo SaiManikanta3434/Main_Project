a74b13e9da8aa22b4b1a55849b81100bd20fbe78
sai-manikanta <116022819+7036143077@users.noreply.github.com>
2025-07-01 10:30:21 +0530

    Modified vercel.json

--- frontend/src/pages/Product.jsx
+++ frontend/src/pages/Product.jsx
@@ -79,9 +79,9 @@ const Product = () => {
       {/* ----- Description & Review Section ----------- */}
 
       <div className='mt-20'>
-        <div className='flex'>
-          <p className='border px-5 py-3 text-sm'>Description</p>
-          <p className='border px-5 py-3 text-sm'>Reviews (122)</p>
+        <div className='flex gap-2'>
+          <p className='border px-5 py-3 text-sm bg-gray-300 rounded-lg'>Description</p>
+          <p className='border px-5 py-3 text-sm bg-gray-300 rounded-lg'>Reviews (122)</p>
         </div>
         <div className='flex flex-col gap-4 border px-6 py-6 text-sm text-gray-500'>
           <p>An e-commerce website is an online platform that facilitates the buying and selling of products or services over the internet. It serves as a virtual marketplace where business and individuals can showcase their products, interact with customers, and conduct transactions without the need for a physical presence. E-Commerce websites have gaines an immense popularity due to their convenience, accessibility, and the global reach they offer. </p>
